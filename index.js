6699fbff93c4c27af7878aefae8248c2dc1d5100
omarayman639 <56203198+omarayman639@users.noreply.github.com>
2020-10-23 16:32:53 +0200

    added html

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const express = require('express')
 const app = express()
-app.get('/', (req, res) => {
-  res.send('HEY! omar ayman shokry 222')
-})
+app.use(express.static('public'))
 app.listen(3000, () => console.log('Server running on port 3000'))
