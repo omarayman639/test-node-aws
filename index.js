3560b08c357d1cd25427eb4857b96b9f582fedbb
omarayman639 <56203198+omarayman639@users.noreply.github.com>
2020-10-23 16:23:09 +0200

    u

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
 app.get('/', (req, res) => {
-  res.send('HEY! omar ayman shokry')
+  res.send('HEY! omar ayman shokry 222')
 })
 app.listen(3000, () => console.log('Server running on port 3000'))
