910b2df1d0a5958aebffef74d72187441f47f3c2
omarayman639 <56203198+omarayman639@users.noreply.github.com>
2020-10-23 16:11:05 +0200

    u

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,11 +6,12 @@ module.exports = {
     deploy: {
       production: {
         user: 'ubuntu',
-        host: 'ec2-3-138-191-238.us-east-2.compute.amazonaws.com/',
-        key: '~/.ssh/tutorial-2.pem',
+        host: 'ec2-3-138-191-238.us-east-2.compute.amazonaws.com',
+        key: 'D:/Development/test-aws/test-node-key.pem',
         ref: 'origin/master',
         repo: 'https://github.com/omarayman639/test-node-aws.git',
-        path: '/home/ubuntu/test-node-2',
+        path: '/home/ubuntu/node-test-2',
+        ssh_options: 'ForwardAgent=yes',
         'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
       }
     }
